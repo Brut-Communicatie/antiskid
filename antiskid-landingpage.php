84800e0f316e9af2d900623a3a4b88159fcb6f3a
TMB <tmb@TMB-1.local>
2021-03-15 14:47:45 +0100

    design

---
+++ antiskid-landingpage.php
@@ -0,0 +1,43 @@
+<?php 
+
+//Template name: Landingpage
+
+get_header();?>
+
+<div class="banner">
+    <div class="banner__background"></div>
+    <div class="banner__container">
+        <div class="banner__heading">
+            <h1>Get a grip on your </br> turnover with Antiskid®</h1>
+            <a href="#" class="button__orange">Apply for a free consultation</a>
+            <p>or call <a href="#" class="button__animated">0031 1234 5678</a></p>
+        </div>
+        <div class="banner__image">
+        </div>
+    </div>
+</div>
+
+<div class="content">
+    <div class="content__container">
+        <div class="content__left">
+        <h2>POSSEHL Antiskid®</h2>
+        <h3>The best solution for airports to operate more efficient, sustainable and safe.</h3>
+        <p>Sustainability is becoming more important and weather conditions are becoming more extreme. Critical to landing and takeoff safety is the traction of the landing gear tires on the runway surface. The high-friction properties of Antiskid significantly reduce runway occupation time, while increasing safety and capacity and, thereby, profitability.</p>
+        </div>
+
+        <div class="content__right">
+            <div class="block">
+                <h4>POSSEHL Antiskid® for:</h4>
+                <ul>
+                    <li>Maximized operations</li>
+                    <li>Safety in all (extreme) climatic conditions</li>
+                    <li>Sustainable runways</li>
+                    <li>Doubled runway lifecycle</li>
+                    <li>Savings on OPEX</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php get_footer();?>
--- header.php
+++ header.php
@@ -25,35 +25,16 @@
 <div id="page" class="site">
 	<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'antiskid' ); ?></a>
 
-	<header id="masthead" class="site-header">
-		<div class="site-branding">
-			<?php
-			the_custom_logo();
-			if ( is_front_page() && is_home() ) :
-				?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-				<?php
-			else :
-				?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-				<?php
-			endif;
-			$antiskid_description = get_bloginfo( 'description', 'display' );
-			if ( $antiskid_description || is_customize_preview() ) :
-				?>
-				<p class="site-description"><?php echo $antiskid_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
-			<?php endif; ?>
-		</div><!-- .site-branding -->
-
-		<nav id="site-navigation" class="main-navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'antiskid' ); ?></button>
-			<?php
-			wp_nav_menu(
-				array(
-					'theme_location' => 'menu-1',
-					'menu_id'        => 'primary-menu',
-				)
-			);
-			?>
-		</nav><!-- #site-navigation -->
+	<header id="masthead" class="header">
+	<div class="header__top">
+		<div class="header__top--logo"></div>
+	</div>
+	<div class="header__main">
+		<div class="header__container">
+			<div class="header__main--nav">	
+				<a href="#" class="button__animated">0031 1234 5678</a>
+				<a class="button__orange" href="#">Contact</a>
+			</div>
+		</div>
+	</div>
 	</header><!-- #masthead -->
