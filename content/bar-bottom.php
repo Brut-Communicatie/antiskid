df20d35fb7c9f3abc5128b7b7126dc2539b172d7
TMB <tmb@TMB-1.local>
2021-03-17 15:39:48 +0100

    fix

--- content/bar-bottom.php
+++ content/bar-bottom.php
@@ -2,10 +2,9 @@
 $tel = "0049 670 120 449 26";
 $callto = str_replace(' ', '', $tel);
 ;?>
-
+<div class="bottom__section">
 <section class="bottom"  style="background-image: url('<?php echo get_template_directory_uri();?>/images/footer.png');" id="contact">
     <div class="bottom__background"></div>
-
     <div class="bottom__container">
         <div class="bottom__left">
         <h4>Need some advice?</h4>
@@ -19,6 +18,5 @@ $callto = str_replace(' ', '', $tel);
             <img src="<?php echo get_template_directory_uri();?>/images/person.png" width="300px" alt="Guy" />
         </div>
     </div>
-
 </section>
-<div class="bottom__bar"></div>
+</div>
