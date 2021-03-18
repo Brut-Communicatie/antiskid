213e8a5a0023fa83a04b51f472501323073ef88c
Leroy Davidson Descelles <leroy.davidsondescelles@zoolt.com>
2021-03-18 13:37:48 +0100

    fix

--- content/intro-left.php
+++ content/intro-left.php
@@ -7,7 +7,8 @@
 </p>
 
 <div class="content__video">
-<h4>Curious to see all ins and outs of Possehl Antiskid®? </br><strong>Check out this video animation below.</strong></h4>
+<h4>Curious to see all ins and outs of Possehl Antiskid®? </br><strong>Check out this video animation.</strong></h4>
+<p>Antiskid® is a rigid surface that is applied over runway asphalt. The surface layer is similar to coarse sandpaper. It consists mainly of epoxy and sharp basalt. ANTISKID® minimizes the rolling resistance and loss of traction during acceleration on wet runways, while vastly improving directional stability, in particular, in crosswinds.</p>
 <video controls poster="<?php echo get_template_directory_uri();?>/video/poster.jpg">
 <source src="<?php echo get_template_directory_uri();?>/possehl.webm"
             type="video/webm">
@@ -15,7 +16,6 @@
     <source src="<?php echo get_template_directory_uri();?>/video/possehl.mp4"
             type="video/mp4">
 </video>
-<p>Antiskid® is a rigid surface that is applied over runway asphalt. The surface layer is similar to coarse sandpaper. It consists mainly of epoxy and sharp basalt.</p>
 </div>
 
 <div class="content__bottom">
