6eb292148c4bdda7a4bd32afaec4a219cf8b6d72
Leroy Davidson Descelles <leroy@Leroys-MacBook-Pro.local>
2021-04-16 13:14:30 +0200

    contactform

--- js/contactform.js
+++ js/contactform.js
@@ -1,5 +1,5 @@
 console.log('Contact form GA listener loaded - Brut Communicatie');
-//Add event listener to a succesfull form submission
+//Add event listener to a  succesful form submission
 document.addEventListener( 'wpcf7mailsent', function( event ) {
     //Send the information to Google Analytics
     ga( 'send', 'event', 'Contact Form', 'submit' ); 
