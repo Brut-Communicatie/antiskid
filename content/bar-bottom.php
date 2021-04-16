1293c7a0cf9b492555462d2492c753cd5a4fdb12
Leroy Davidson Descelles <leroy@Leroys-MacBook-Pro.local>
2021-04-16 10:09:57 +0200

    shortcode

--- content/bar-bottom.php
+++ content/bar-bottom.php
@@ -1,6 +1,8 @@
 <?php
 $tel = "0049 670 120 449 26";
 $callto = str_replace(' ', '', $tel);
+
+$shortcode = get_field('shortcode');
 ;?>
 
 <section class="bottom"  style="background-image: url('<?php echo get_template_directory_uri();?>/images/footer.png');" id="contact">
@@ -11,7 +13,7 @@ $callto = str_replace(' ', '', $tel);
         <h4>Need advice?</h4>
         <h3>Get in touch with our experts!</h3>
         <div class="contact">
-            <?php echo do_shortcode('[contact-form-7 id="7" title="Contactform"]');?>
+            <?php echo do_shortcode($shortcode);?>
         </div>
         <p>or call <a href="tel:<?php echo $callto;?>" class="button__animated"><img src="<?php echo get_template_directory_uri();?>/images/phone.svg" width="20px" alt="phone" /> <?php echo $tel;?></a></p>
         </div>
