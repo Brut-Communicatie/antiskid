93729359a27ba1295eb3c56af3809a91065460a7
Leroy Davidson Descelles <leroy@Leroys-MacBook-Pro.local>
2021-04-16 10:20:49 +0200

    responsive fixes

--- content/bar-bottom.php
+++ content/bar-bottom.php
@@ -7,15 +7,14 @@ $shortcode = get_field('shortcode');
 
 <section class="bottom"  style="background-image: url('<?php echo get_template_directory_uri();?>/images/footer.png');" id="contact">
     <div class="bottom__background"></div>
-
     <div class="bottom__container">
         <div class="bottom__left">
-        <h4>Need advice?</h4>
-        <h3>Get in touch with our experts!</h3>
+            <h4>Need advice?</h4>
+            <h3>Get in touch with our experts!</h3>
         <div class="contact">
             <?php echo do_shortcode($shortcode);?>
         </div>
-        <p>or call <a href="tel:<?php echo $callto;?>" class="button__animated"><img src="<?php echo get_template_directory_uri();?>/images/phone.svg" width="20px" alt="phone" /> <?php echo $tel;?></a></p>
+            <p>or call <a href="tel:<?php echo $callto;?>" class="button__animated"><img src="<?php echo get_template_directory_uri();?>/images/phone.svg" width="20px" alt="phone" /> <?php echo $tel;?></a></p>
         </div>
         <div class="bottom__right">
             <img src="<?php echo get_template_directory_uri();?>/images/person.png" width="300px" alt="Guy" />
@@ -24,8 +23,8 @@ $shortcode = get_field('shortcode');
 
 </section>
 <div class="bottom__bar">
-<div class="bottom__bar--container">
-<a href="https://www.possehl.de/en/data-protection/" target="_blank" rel="noopener noreferrer">Privacystatement</a>
-<a href="https://www.possehl.de/en/" target="_blank" rel="noopener noreferrer">Go to possehl.de</a>
-</div>
+    <div class="bottom__bar--container">
+        <a href="https://www.possehl.de/en/data-protection/" target="_blank" rel="noopener noreferrer">Privacystatement</a>
+        <a href="https://www.possehl.de/en/" target="_blank" rel="noopener noreferrer">Go to possehl.de</a>
+    </div>
 </div>
